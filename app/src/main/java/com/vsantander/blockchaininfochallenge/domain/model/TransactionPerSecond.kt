96ca8638f27647c301f4abe9d7fba944521bde9d
Víctor Santander <victor.santander@pangeareality.com>
2018-10-05 17:18:04 +0100

    last changes

--- app/src/main/java/com/vsantander/blockchaininfochallenge/domain/model/TransactionPerSecond.kt
+++ app/src/main/java/com/vsantander/blockchaininfochallenge/domain/model/TransactionPerSecond.kt
@@ -1,6 +1,10 @@
 package com.vsantander.blockchaininfochallenge.domain.model
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class TransactionPerSecond(
         val timestamp: Float,
         val numberBitcoins: Float
-)
+) : Parcelable
